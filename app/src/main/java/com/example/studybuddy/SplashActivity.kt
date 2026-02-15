50b666018a9a6a83d881e3cf4d148d191500dd68
gibgibbb <diazkristian321@gmail.com>
2026-02-15 22:16:42 +0800

    Added Intent on Splash -> Login -> Dashboard

--- app/src/main/java/com/example/studybuddy/SplashActivity.kt
+++ app/src/main/java/com/example/studybuddy/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.example.studybuddy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,11 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+        val getStartedBtn = findViewById<Button>(R.id.button)
+        getStartedBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
